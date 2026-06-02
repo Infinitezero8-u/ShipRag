6e4887e538b87eb83cfa19ad4070b2c4ad3d7697
user8588222836 <344609882652347-user8588222836@noreply.coze.cn>
2026-06-03 06:48:01 +0800

    feat: 创建 HelloWorld 示例项目
    Coze-Commit-Type: user
    Coze-User-ID: 344609882652347
    Coze-Conversation-ID: 6202691

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+HelloWorld 示例项目
+"""
+
+def hello():
+    """打印 Hello World"""
+    print("Hello, World!")
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    hello()
